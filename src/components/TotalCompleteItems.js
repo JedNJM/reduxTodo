8d52e14de6eae11f34f0e53c43f41df2b4b565dc
MedJedNJM <81960158+JedNJM@users.noreply.github.com>
2023-03-04 22:57:42 +0100

    hello

--- src/components/TotalCompleteItems.js
+++ src/components/TotalCompleteItems.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 const TotalCompleteItems = () => {
 	const completedTodo = useSelector((state)=> state.todos.filter((done)=> done.completed === true))
-	return <h4 className='mt-3'>Completed: {completedTodo.length}</h4>;
+	return <h4 className='mt-3'>Completed: {completedTodo.length} hello</h4>;
 };
 
 export default TotalCompleteItems;
